d2cc8ef016f02d8cbdc300032c1bc9d0702b6961
ryeballar@gmail.com <Raizel10162009>
2013-12-16 22:05:49 +0800

    Latest

--- application/views/profile/image.php
+++ application/views/profile/image.php
@@ -1,4 +1,6 @@
-<img src="<?php echo $user_image ?>" style="width: 200px; height: 180px;" /><br><br>
+<a class="th" href="<?php echo $user_image ?>">
+	<img src="<?php echo $user_image ?>" style="width: 200px; height: 180px;" />
+</a><br><br>
 <form action="upload_avatar" method="post" accept-charset="utf-8" enctype="multipart/form-data">
 	<input type="file" name="userfile" style="font-size: 15px;"/>
 	<button class="tiny radius" type="submit">Upload</button>
--- application/views/profile/view.php
+++ application/views/profile/view.php
@@ -4,7 +4,8 @@
 		<div class="large-3 columns"><?php $this->layout->view('image') ?></div>
 		<div class="large-6 columns" style="overflow: hidden"><?php $this->layout->view('details') ?></div>
 		<div class="large-3 columns"><?php $this->layout->view('total_stats') ?></div>
-	</div><hr>
+		<hr>
+	</div>
 	<section class="text-center">
 		<img data-src="holder.js/800x150/text:Graph Here for Activities">
 	</section><br><br><br>
