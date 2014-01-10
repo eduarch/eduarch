095ccbd40cf382e2ba8e1497a86bede9b3771f07
ryeballar@gmail.com <ryeballar@gmail.com>
2014-01-10 12:56:56 +0800

    ..

--- application/models/tutorial_model.php
+++ application/models/tutorial_model.php
@@ -1,9 +1,9 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once 'Abstract_Model.php';
+require_once 'abstract_model.php';
 
 class tutorial_model extends Abstract_Model {
-	function __construct() {
+	function __construct() { 
 		parent::__construct('tutorials', 'id', 'tutorial_id');
 	}
 }
